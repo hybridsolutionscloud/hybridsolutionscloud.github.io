b36c37d2307627cc10db564f76e9fae06f654259
Kris Turner <kristopherjturner@outlook.com>
2025-05-22 20:22:15 -0400

    logo changes

--- src/config/settings.js
+++ src/config/settings.js
@@ -2,7 +2,7 @@ export default {
 	title: `Hybrid Cloud Solutions | Hybrid Cloud Made Simple Built On Microsoft Technologies`,
 	description: `From Azure Local to Hyper-V and Azure Arc — grounded, resilient hybrid solutions built for the edge and the enterprise.`,
 	url: `https://www.hybridsolutions.cloud`, // No trailing slash!
-	name: `Hybrid Solutions`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `Hybrid Cloud Solutions`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: true,
 	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
